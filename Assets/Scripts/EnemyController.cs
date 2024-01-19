c8c6e89d0f1a0a196de9a3b2e4c5856cc3554691
bigmmg205 <146846706+bigmmg205@users.noreply.github.com>
2024-01-19 09:00:59 -0500

    Initial commit

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// This class handle Enemy behaviour. It make them walk back & forth as long as they aren't fixed, and then just idle
+/// without being able to interact with the player anymore once fixed.
+/// </summary>
+public class EnemyController : MonoBehaviour
+{
+	// ====== ENEMY MOVEMENT ========
+	public float speed;
+	public float timeToChange;
+	public bool horizontal;
+
+	
+	Rigidbody2D rigidbody2d;
+	float remainingTimeToChange;
+	Vector2 direction = Vector2.right;
+
+
+	bool broken=true;
+	
+	// ===== ANIMATION ========
+	Animator animator;
+	
+	// ================= SOUNDS =======================
+
+	void Start ()
+	{
+		rigidbody2d = GetComponent<Rigidbody2D>();
+		remainingTimeToChange = timeToChange;
+
+		animator = GetComponent<Animator>();
+
+		direction = horizontal ? Vector2.right : Vector2.down;
+	}
+	
+	void Update()
+	{
+		if(!broken)
+		return;
+		remainingTimeToChange -= Time.deltaTime;
+
+		if (remainingTimeToChange <= 0)
+		{
+			remainingTimeToChange += timeToChange;
+			direction *= -1;
+		}
+
+		animator.SetFloat("Move X", direction.x);
+		animator.SetFloat("Move Y", direction.y);
+	}
+
+	void FixedUpdate()
+	{
+		rigidbody2d.MovePosition(rigidbody2d.position + direction * speed * Time.deltaTime);
+	}
+
+	void OnCollisionStay2D(Collision2D other)
+	{
+		RubyController controller = other.collider.GetComponent<RubyController>();
+		
+		if(controller != null)
+		{
+			controller.ChangeHealth(-1);
+		}
+			
+	}
+
+	//Public because we want to call it from elsewhere like the projectile script
+		public void Fix()
+		{
+			broken = false;
+			rigidbody2d.simulated = false;
+		}
+
+
+}
---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    // ========= MOVEMENT =================
+    public float speed = 4;
+    
+    // ======== HEALTH ==========
+    public int maxHealth = 5;
+    
+    
+    // ======== HEALTH ==========
+    public int health
+    {
+        get { return currentHealth; }
+    }
+    
+    // =========== MOVEMENT ==============
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    
+    
+    // ======== HEALTH ==========
+    int currentHealth;
+
+    // ======== Health Timer =========
+    float invincibleTimer;
+    bool isInvincible;
+    public float timeInvincible = 2.0f;
+
+    // ========== Animator ==========
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    public GameObject projectilePrefab;
+
+
+    
+
+    void Start()
+    {
+        // =========== MOVEMENT ==============
+        rigidbody2d = GetComponent<Rigidbody2D>();
+                
+        // ======== HEALTH ==========
+        currentHealth = maxHealth;
+
+        // ========== Animator ==========
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // ============== MOVEMENT ======================
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        // ============ Where the player is looking ==========
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(0.0f, move.y))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        // ================= HEALTH TIMER ====================
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    // ===================== HEALTH ==================
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        { 
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+
+      
+    }
+
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
+    }
+
+
+
+}
